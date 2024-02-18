fed2ef22a0f22ca08d95c546a068c33dc39bd75e
BuildTools <skywarspanel@gmail.com>
2024-02-18 13:42:48 +0100

    Initial commit

---
+++ src/main/kotlin/me/lished/rarity/Rarity.kt
@@ -0,0 +1,13 @@
+package me.lished.rarity
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Rarity : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
