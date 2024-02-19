fdc3697ffdb0ed054450bbf7dbbb6bc7232992d3
BuildTools <skywarspanel@gmail.com>
2024-02-19 16:57:37 +0100

    remove some symbols

--- src/main/java/me/lished/rarity/listeners/RarityListener.kt
+++ src/main/java/me/lished/rarity/listeners/RarityListener.kt
@@ -17,7 +17,7 @@ class RarityListener : Listener {
             val item = e.currentItem
 
             if (item != null && item.type != Material.AIR && item.lore() == null) {
-                item.lore(listOf(Component.text("§r "), Component.text(item.rarity.name, TextColor.color(item.rarity.color)).decorate(TextDecoration.BOLD)))
+                item.lore(listOf(Component.text(""), Component.text("§r"+item.rarity.name, TextColor.color(item.rarity.color)).decorate(TextDecoration.BOLD)))
             }
         }
     }
