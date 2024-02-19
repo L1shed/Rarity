7aec50adebd800694f248a86d41cd392b1e535a7
BuildTools <skywarspanel@gmail.com>
2024-02-19 16:41:14 +0100

    New options

--- src/main/java/me/lished/rarity/listeners/RarityListener.kt
+++ src/main/java/me/lished/rarity/listeners/RarityListener.kt
@@ -15,7 +15,7 @@ class RarityListener : Listener {
             val item = e.currentItem
 
             if (item != null && item.type != Material.AIR && item.lore() == null) {
-                item.lore(listOf(Component.newline(), Component.text("Rarity: " + item.rarity.name+item.rarity.ordinal).color(item.rarity.color)))
+                item.lore(listOf(Component.newline(), Component.text("§r§b" + item.rarity.name).color(item.rarity.color)))
             }
         }
     }
